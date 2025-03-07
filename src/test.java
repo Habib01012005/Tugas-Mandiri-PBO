3b010d7b4978302fa0458deaf835006d0315eab8
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-03-07 14:53:44 +0800

    Add files via upload

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import javax.xml.crypto.Data;
+
+public class test {
+    public static void main(String[] args) {
+        DataBase db = new DataBase();
+        db.open();
+        db.view();
+    }
+}
